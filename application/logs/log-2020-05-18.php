aaa05b2715279a15c798752eff86b1bacc08e32b
GagaPoloJr <rahmadiihaikal@gmail.com>
2020-05-18 13:20:56 +0700

    fd

---
+++ application/logs/log-2020-05-18.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-05-18 08:19:33 --> 404 Page Not Found: Js/jquery-3.4.1.min.js
+ERROR - 2020-05-18 08:19:44 --> 404 Page Not Found: Page/js
+ERROR - 2020-05-18 08:19:44 --> 404 Page Not Found: Page/js
+ERROR - 2020-05-18 08:19:44 --> 404 Page Not Found: Page/js
+ERROR - 2020-05-18 08:20:21 --> 404 Page Not Found: Page/js
+ERROR - 2020-05-18 08:20:21 --> 404 Page Not Found: Page/js
+ERROR - 2020-05-18 08:20:22 --> 404 Page Not Found: Page/js
